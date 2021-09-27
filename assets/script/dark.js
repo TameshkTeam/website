ab2acc53259e73881f1e34bc74cdaf61c953dcfd
sina-yeganeh <sinayeganeh.dev@gmail.com>
2021-09-27 14:33:31 +0330

    Add copy right to top of styles and js scripts

--- assets/script/dark.js
+++ assets/script/dark.js
@@ -1,7 +1,11 @@
+/* 
+  * (C) 2021 Sina yeganeh  
+  *
+  * Github: https://github.com/sina-yeganeh
+*/
+
 var darkButton = document.querySelector(".dark")
 var body = document.querySelector("body")
-var navBar = document.querySelector("nav")
-var theme = localStorage.getItem("theme")
 
 darkButton.addEventListener("click", () => {
   body.classList.toggle("dark-mode")
